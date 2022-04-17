036636a5be8feba30bef490d61511757b6edd233
Avior Kahalani <59415129+AviorKahalani@users.noreply.github.com>
2022-04-17 13:24:45 +0300

    added demo routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Component } from 'react'
 import './styles/styles.css'
 
 import { AppHeader } from './components/AppHeader'
@@ -6,16 +7,28 @@ import { AppFooter } from './components/AppFooter'
 import { HomePage } from './views/HomePage'
 import { StatisticPage } from './views/StatisticPage'
 
-export default function App() {
-  return (
-    <section className="app-container container flex flex-col">
-      <AppHeader />
-      <main className="flex-1">
-        <HomePage />
-        {/* <ContactApp /> */}
-        {/* <StatisticPage /> */}
-      </main>
-      <AppFooter />
-    </section>
-  )
+export default class App extends Component {
+  state = {
+    page: null,
+  }
+
+  onChangePage = (page) => {
+    this.setState({ page })
+  }
+
+  render() {
+    const { page } = this.state
+
+    return (
+      <section className="app-container container flex flex-col">
+        <AppHeader onChangePage={this.onChangePage} />
+        <main className="flex-1">
+          {!page && <HomePage />}
+          {page === 'contact' && <ContactApp />}
+          {page === 'about' && <StatisticPage />}
+        </main>
+        <AppFooter />
+      </section>
+    )
+  }
 }
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,13 +1,19 @@
-export function AppHeader() {
+export function AppHeader({ onChangePage }) {
   return (
     <header className="main-header flex justify-between items-center">
       <h1 className="logo">
         Mister<span className="fw-bold clr-teal">Bitcoin</span>
       </h1>
       <nav className="nav flex gap-2">
-        <div className="link">Home</div>
-        <div className="link">Contacts</div>
-        <div className="link">About</div>
+        <div onClick={() => onChangePage(null)} className="link">
+          Home
+        </div>
+        <div onClick={() => onChangePage('contact')} className="link">
+          Contacts
+        </div>
+        <div onClick={() => onChangePage('about')} className="link">
+          About
+        </div>
       </nav>
     </header>
   )
